b528203183cca646800e1b1dd937096bfb80c490
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-04-29 11:39:01 +0200

    create

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const myspl2 = require("mysql2/promise");
+const mysql = require("mysql2/promise");
 
 const app = express();
 
@@ -21,4 +21,89 @@ app.use(express.json());
 const port = 5001;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+// C /driver + /results
+app.post("/drivers", async (req, res) => {
+    const connection = await getDBConnection();
+    const { first_name, last_name, nationality, car_number, team } = req.body;
+
+    if (!first_name || !last_name || !nationality || !car_number || !team) {
+        res.status(400).json({
+            success: false,
+            message:
+                "Bad params. Provide 'first_name', 'last_name', 'nationality', 'car_number', and 'team'",
+        });
+        return;
+    }
+
+    try {
+        const sql =
+            "INSERT INTO drivers (first_name, last_name, nationality, car_number, team) VALUES (?, ?, ?, ?, ?)";
+        const [result] = await connection.query(sql, [
+            first_name,
+            last_name,
+            nationality,
+            car_number,
+            team,
+        ]);
+        await connection.end();
+
+        res.status(201).json({
+            success: true,
+            message: "Driver added",
+            id: result.insertId,
+        });
+    } catch (error) {
+        await connection.end();
+        res.status(500).json({
+            success: false,
+            message: "Database error",
+            error: error.message,
+        });
+    }
+});
+
+app.post("/results", async (req, res) => {
+    const connection = await getDBConnection();
+    const { position, car_number, race_time, points } = req.body;
+
+    if (
+        position === undefined ||
+        !car_number ||
+        !race_time ||
+        points === undefined
+    ) {
+        res.status(400).json({
+            success: false,
+            message:
+                "Bad params. Provide 'position', 'car_number', 'race_time', and 'points'",
+        });
+        return;
+    }
+
+    try {
+        const sql = `INSERT INTO bahrain_2025_results 
+        (position, car_number, race_time, points) 
+        VALUES (?, ?, ?, ?)`;
+        const [result] = await connection.query(sql, [
+            position,
+            car_number,
+            race_time,
+            points,
+        ]);
+        await connection.end();
+
+        res.status(201).json({
+            success: true,
+            message: "Race result added",
+        });
+    } catch (error) {
+        await connection.end();
+        res.status(500).json({
+            success: false,
+            message: "Database error",
+            error: error.message,
+        });
+    }
 });
